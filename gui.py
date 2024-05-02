2aed16736e9623fe6029678f0c6a118bb3117f4a
samantinoro <samantinoro@unomaha.edu>
2024-05-02 00:00:08 -0500

    gui

--- gui.py
+++ gui.py
@@ -1,2 +1,9 @@
 # gui
+from tkinter import *
+import logic
+
 class Gui:
+    def __init__(self, window):
+        # Construct initial window
+        self.window = window
+        self.frame_shape = Frame(self.window)
